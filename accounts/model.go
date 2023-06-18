22434604a0e3121ddae767983002a52af2472e57
efen stakes <efenstakes101@gmail.com>
2023-06-18 12:08:52 +0300

    add an account model

---
+++ accounts/model.go
@@ -0,0 +1,17 @@
+package accounts
+
+import "github.com/kamva/mgm/v3"
+
+type Account struct {
+	mgm.DefaultModel `bson:",inline"`
+
+	// ID          string `json:"id" bson:"_id"`
+	Name       string   `json:"name"`
+	Password   string   `json:"password"`
+	Email      string   `json:"email"`
+	Slug       string   `json:"slug"`
+	JoinedOn   string   `json:"joinedOn" bson:"created_at"`
+	UpdatedOn  string   `json:"updatedOn" bson:"updated_at"`
+	LastActive string   `json:"lastActive" bson:"last_active"`
+	Blocked    []string `json:"blocked" bson:"blocked"`
+}
